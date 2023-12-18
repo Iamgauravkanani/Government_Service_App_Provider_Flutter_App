8dff6ae3c60d5326f578232a42c0434d73dd85c5
Gaurav Kanani <rw4.gaurav.bk@gmail.com>
2023-12-18 11:47:29 +0530

    Web View Implemented

--- lib/App/Views/HomeScreen/Views/homescreen.dart
+++ lib/App/Views/HomeScreen/Views/homescreen.dart
@@ -1,61 +1,116 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  InAppWebViewController? inAppWebViewController;
   Connectivity connectivity = Connectivity();
+  late PullToRefreshController pullToRefreshController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    pullToRefreshController = PullToRefreshController(
+        options: PullToRefreshOptions(
+          color: Colors.black,
+        ),
+        onRefresh: () {
+          inAppWebViewController?.reload();
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     Stream<ConnectivityResult> connectivity_stream =
         connectivity.onConnectivityChanged;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("HomeScreen"),
-      ),
-      body: StreamBuilder(
-        stream: connectivity_stream,
-        builder: (BuildContext ctx, AsyncSnapshot snapshot) {
-          if (snapshot.hasError) {
-            return Center(
-              child: Text("${snapshot.error}"),
+    return SafeArea(
+      child: Scaffold(
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            FloatingActionButton(
+              onPressed: () async {
+                if (await inAppWebViewController!.canGoForward()) {
+                  inAppWebViewController!.goForward();
+                }
+              },
+              child: Icon(Icons.arrow_forward_ios),
+            ),
+            FloatingActionButton(
+              onPressed: () async {
+                if (await inAppWebViewController!.canGoBack()) {
+                  inAppWebViewController!.goBack();
+                }
+              },
+              child: Icon(Icons.arrow_back_ios),
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                inAppWebViewController!.loadUrl(
+                    urlRequest:
+                        URLRequest(url: Uri.parse("https://www.google.com")));
+              },
+              child: Icon(Icons.home),
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                inAppWebViewController?.reload();
+              },
+              child: Icon(Icons.refresh),
+            ),
+          ],
+        ),
+        body: StreamBuilder(
+          stream: connectivity_stream,
+          builder: (BuildContext ctx, AsyncSnapshot snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text("${snapshot.error}"),
+              );
+            } else if (snapshot.hasData) {
+              return (snapshot.data == ConnectivityResult.wifi ||
+                      snapshot.data == ConnectivityResult.mobile)
+                  ? InAppWebView(
+                      pullToRefreshController: pullToRefreshController,
+                      initialUrlRequest:
+                          URLRequest(url: Uri.parse("https://www.google.com")),
+                      onLoadStart: (controller, uri) {
+                        setState(() {
+                          inAppWebViewController = controller;
+                        });
+                      },
+                      onLoadStop: (controller, uri) {
+                        pullToRefreshController.endRefreshing();
+                      },
+                    )
+                  : Center(
+                      child: Container(
+                        height: double.infinity,
+                        width: double.infinity,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(
+                              "lib/App/Utils/Assets/not_connected.jpeg",
+                            ),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    );
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
             );
-          } else if (snapshot.hasData) {
-            return (snapshot.data == ConnectivityResult.wifi ||
-                    snapshot.data == ConnectivityResult.mobile)
-                ? Center(
-                    child: Container(
-                      height: 400,
-                      width: double.infinity,
-                      decoration: const BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage(
-                            "lib/App/Utils/Assets/connected.gif",
-                          ),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  )
-                : Center(
-                    child: Container(
-                      height: double.infinity,
-                      width: double.infinity,
-                      decoration: const BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage(
-                            "lib/App/Utils/Assets/not_connected.jpeg",
-                          ),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  );
-          }
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        },
+          },
+        ),
       ),
     );
   }
